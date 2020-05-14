f43ad45ee07a3f83bb8fe163a716c4cba9a7c4d5
Clara Liu <clara_lzr@hotmail.com>
2020-05-14 17:59:39 +0100

    added data fetch function

---
+++ get_data.js
@@ -0,0 +1,92 @@
+function getFormData() {
+    // initialise datasets for core function
+    var inputValues = {
+        Height: [],
+        Slope: [],
+        Duration: [],
+        Lambda: [],
+    };
+    var initialSettings = {
+        pitch_start:0,
+        bounds:[]
+    };
+    initialSettings.pitch_start = parseFloat(document.getElementById("start_pitch").value);
+    initialSettings.bounds.push(parseFloat(document.getElementById("lower_bound").value));
+    initialSettings.bounds.push(parseFloat(document.getElementById("upper_bound").value));
+
+    var tones = document.getElementById("tone_sequence").value;
+    var durations = document.getElementById("duration_sequence").value;
+    var lambdas = document.getElementById("lambda_sequence").value;
+    if (tones.length!= durations.length || tones.length!= lambdas.length){
+        alert("Sequence length between tones/durations/strengths must be equal!");
+        return;
+    }
+    // record pitch targets specifications
+    for (t=0; t<tones.length; t++){
+        switch(tones[t]){
+            case "H":
+                var height = parseFloat(document.getElementById("h_height").value);
+                var slope = parseFloat(document.getElementById("h_slope").value);
+                break;
+            case "R":
+                var height = parseFloat(document.getElementById("r_height").value);
+                var slope = parseFloat(document.getElementById("r_slope").value);
+                break;
+            case "L":
+                var height = parseFloat(document.getElementById("l_height").value);
+                var slope = parseFloat(document.getElementById("l_slope").value);
+                break;
+            case "F":
+                var height = parseFloat(document.getElementById("f_height").value);
+                var slope = parseFloat(document.getElementById("f_slope").value);
+                break;
+            case "N":
+                var height = parseFloat(document.getElementById("n_height").value);
+                var slope = parseFloat(document.getElementById("n_slope").value);
+                break;
+            default:
+                alert("Please input a valid tone specification!");
+                return;
+        }
+        inputValues.Height.push(height);
+        inputValues.Slope.push(slope);
+    }
+    // record duration specifications
+    for (d=0;d<durations.length;d++){
+        switch (durations[d]) {
+            case "s":
+                var duration = parseFloat(document.getElementById("short").value);
+                break;
+            case "n":
+                var duration = parseFloat(document.getElementById("normal").value);
+                break;
+            case "l":
+                var duration = parseFloat(document.getElementById("long").value);
+                break;
+            default:
+                alert("Please input a valid duration specification!");
+                return;
+        }
+        inputValues.Duration.push(duration);
+    }
+    // record lambda specifications
+    for (l=0;l<lambdas.length;l++){
+        switch (lambdas[l]) {
+            case "w":
+                var lambda = parseFloat(document.getElementById("weak").value);
+                break;
+            case "n":
+                var lambda = parseFloat(document.getElementById("normal").value);
+                break;
+            case "l":
+                var lambda = parseFloat(document.getElementById("long").value);
+                break;
+            default:
+                alert("Please input a valid strength specification!");
+                return;
+        }
+        inputValues.Lambda.push(lambda);
+    }
+    console.log(inputValues);
+    console.log(initialSettings);
+}
--- qta_plot.js
+++ qta_plot.js
@@ -1,3 +1,14 @@
-function qta_plot(){
-    console.log('Triggered!')
+function qta(target_data, initial_data){
+    var time_step = 0.005;
+    var target_num = target_data.Height.length;
+    var total_dur = target_data.Duration.reduce((a,b)=> a+b,0);
+    var sample_num = Math.floor(total_dur/time_step);
+    var grand_time = 0;
+    var f0_data = {
+        Time:[],
+        F0:[],
+        Velocity:[],
+        Acceleration: [],
+    };
+    // for loop for each target to calculate f0_data
 }
